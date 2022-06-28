99326a6697739acd40d43f545bdd8fe33b261948
hrnoksz <harun1973@yahoo.com>
2022-06-28 22:06:45 +0300

    Verification added

--- src/test/java/com/demoblaze/runners/CukesRunner.java
+++ src/test/java/com/demoblaze/runners/CukesRunner.java
@@ -8,9 +8,10 @@ import org.junit.runner.RunWith;
 @RunWith(Cucumber.class)
 @CucumberOptions(
         plugin = {
-                "html:target/cucumber-report.html"
+                "html:target/cucumber-report.html",
+                "json:target/cucumber.json"
         },
-        features = "src/test/resources/features/test.feature",
+        features = "src/test/resources/features",
         glue = "com/demoblaze/step_definitions",
         dryRun = false,
         tags = "@wip"
